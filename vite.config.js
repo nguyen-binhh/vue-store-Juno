980ff74f061ce043521781a059e752ec5d621ee2
hikaruti <nguyenphubinh2207@gmail.com>
2023-02-15 19:45:56 +0700

    fix vitejs

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,19 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // server: {
+  //   proxy: {
+  //     "/": {
+  //       target: "http://localhost:3000/api/v1",
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      // https://rollupjs.org/guide/en/#big-list-of-options
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "vue",
-        },
-      },
     },
   },
 });
